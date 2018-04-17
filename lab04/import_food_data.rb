f4dd84cfef2ccc6af5da80ca202cc7fbf0456d82
Jordan Mohler <jordan.mohler@du.edu>
2018-04-16 18:59:32 -0700

    Question 6 part 4 updates

--- lab04/import_food_data.rb
+++ lab04/import_food_data.rb
@@ -24,8 +24,8 @@ while reader.has_next
   if type == XMLStreamConstants::START_ELEMENT
 
     case reader.local_name
-    when 'Food_Display_Row' then document = {}
-    when /food_code|display_name|portion_amount|portion_display_name/ then buffer = []
+    when 'Food_Display_Table' then document = {}
+    when /Food_Code|Display_Name|Portion_Default|Portion_Amount|Portion_Display_Name|Factor|Increment|Multiplier|Grains|Whole_Grains|Vegetables|Orange_Vegetables|Drkgreen_Vegetables|Starchy_vegetables|Other_Vegetables|Fruits|Milk|Meats|Soy|Drybeans_Peas|Oils|Solid_Fats|Added_Sugars|Alcohol|Calories|Saturated_Fats/ then buffer = []
     end
 
   elsif type == XMLStreamConstants::CHARACTERS
@@ -35,21 +35,43 @@ while reader.has_next
   elsif type == XMLStreamConstants::END_ELEMENT
 
     case reader.local_name
-    when /food_code|display_name|portion_amount|portion_display_name/
+    when /Food_Code|Display_Name|Portion_Default|Portion_Amount|Portion_Display_Name|Factor|Increment|Multiplier|Grains|Whole_Grains|Vegetables|Orange_Vegetables|Drkgreen_Vegetables|Starchy_vegetables|Other_Vegetables|Fruits|Milk|Meats|Soy|Drybeans_Peas|Oils|Solid_Fats|Added_Sugars|Alcohol|Calories|Saturated_Fats/
       document[reader.local_name] = buffer.join
     when 'Food_Display_Row'
       key = document['food_code'].to_i
 
       p = Put.new(key)
       p.add(*jbytes("facts", "name", document['display_name']))
+      p.add(*jbytes("facts", "portion default", document['portion_default']))
       p.add(*jbytes("facts", "portion amount", document['portion_amount']))
-      p.add(*jbytes("facts", "portion type", document['portion_display_name']))
+      p.add(*jbytes("facts", "portion display name", document['portion_display_name']))
+      p.add(*jbytes("facts", "factor", document['factor']))
+      p.add(*jbytes("facts", "increment", document['increment']))
+      p.add(*jbytes("facts", "multiplier", document['multiplier']))
+      p.add(*jbytes("facts", "grains", document['grains']))
+      p.add(*jbytes("facts", "whole grains", document['whole_grains']))
+      p.add(*jbytes("facts", "vegetables", document['vegetables']))
+      p.add(*jbytes("facts", "orange vegetables", document['orange_vegetables']))
+      p.add(*jbytes("facts", "dark green vegetables", document['drkgreen_vegetables']))
+      p.add(*jbytes("facts", "starchy vegetables", document['starchy_vegetables']))
+      p.add(*jbytes("facts", "other vegetables", document['other_vegetables']))
+      p.add(*jbytes("facts", "fruits", document['fruits']))
+      p.add(*jbytes("facts", "milk", document['milk']))
+      p.add(*jbytes("facts", "meats", document['meats']))
+      p.add(*jbytes("facts", "soy", document['soy']))
+      p.add(*jbytes("facts", "drybeans peas", document['Drybeans_Peas']))
+      p.add(*jbytes("facts", "oils", document['Oils']))
+      p.add(*jbytes("facts", "solid fats", document['Solid_Fats']))
+      p.add(*jbytes("facts", "added sugars", document['Added_Sugars']))
+      p.add(*jbytes("facts", "alcohol", document['Alcohol']))
+      p.add(*jbytes("facts", "calories", document['Calories']))
+      p.add(*jbytes("facts", "saturated fats", document['Saturated_Fats']))
       table.put(p)
 
       count += 1
       table.flushCommits() if count % 10 == 0
       if count % 500 == 0
-        puts "#{count} records inserted (#{document['display_name']})"
+        puts "#{count} records inserted (#{document['food_code']})"
       end
     end
   end
