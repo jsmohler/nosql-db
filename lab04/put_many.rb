25bb9daedcf1e35d43dd6d06930e809fad8d206c
Victoria Fernalld <victoria.fernalld@yahoo.com>
2018-04-17 14:42:13 -0700

    Final

--- lab04/put_many.rb
+++ lab04/put_many.rb
@@ -6,12 +6,13 @@ def jbytes(*args)
 end
 
 def put_many(table_name, row, column_values)
-    HTable table = new HTable(@hbase.configuration, table_name)
+
+    table = HTable.new(@hbase.configuration, table_name)
 
     p = Put.new(*jbytes(row))
     column_values.each do |key, value|
     value_array = key.split(':')
     p.add(*jbytes(value_array[0], value_array[1], value))
-
+end 
     table.put(p)
 end
