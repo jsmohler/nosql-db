522e93ae39888bf9a297c8a52c831a8bb06bd44f
Victoria Fernalld <victoria.fernalld@yahoo.com>
2018-04-17 13:37:44 -0700

    final additions

--- lab04/put_many.rb
+++ lab04/put_many.rb
@@ -1,4 +1,13 @@
-def put_many(table_name, row, column_values)
-    #code here
+import 'org.apache.hadoop.hbase.client.HTable'
+import 'org.apache.hadoop.hbase.client.Put'
 
+def put_many(table_name, row, column_values)
+    HTable table = new HTable(@hbase.configuration, table_name)
+
+    p = Put.new(*jbytes(row))
+    column_values.each do |key, value|
+    value_array = key.split(':')
+    p.add(*jbytes(value_array[0], value_array[1], value))
+
+    table.put(p)
 end
