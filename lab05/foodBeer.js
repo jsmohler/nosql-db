98d3f8bc11edc9c1a8277b47f16affdd61de28e1
Victoria Fernalld <victoria.fernalld@yahoo.com>
2018-04-20 12:41:22 -0700

    additions

--- lab05/foodBeer.js
+++ lab05/foodBeer.js
@@ -1,3 +1,5 @@
+//Note to Jordan: I feel like I should include the commented out section, but I error when I include that.  And the code as is runs and gets the right stuff.  Thoughts?
+
 var beerAndFood = {
 //$elemMatch : {
 name : /e/i, 
