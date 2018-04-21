f850c37cc647054012ce0af260a19b0be90ec227
Victoria Fernalld <victoria.fernalld@yahoo.com>
2018-04-21 16:57:12 -0700

    modified beerFood.js

--- lab05/foodBeer.js
+++ lab05/foodBeer.js
@@ -1,10 +1,6 @@
-//Note to Jordan: I feel like I should include the commented out section, but I error when I include that.  And the code as is runs and gets the right stuff.  Thoughts?
-
 var beerAndFood = {
-//$elemMatch : {
 name : /e/i, 
 famousFor : /food|beer/i
-//}
 }
 
 db.towns.find(beerAndFood);
