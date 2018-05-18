f518d76f14a2c7fd420a9ccd940a1287153d8191
Victoria Fernalld <victoria.fernalld@yahoo.com>
2018-05-18 14:30:50 -0700

    wtf

--- project1/postgres/takeOrder.sql
+++ project1/postgres/takeOrder.sql
@@ -1,46 +1,40 @@
-
-CREATE OR REPLACE FUNCTION takeOrder(userName integer, recipe varchar(30)) RETURNS boolean AS $$
-
+CREATE OR REPLACE FUNCTION takeOrder(userName integer, recipe varchar(30)) RETURNS void AS $$
 
 DECLARE
-	_userID integer;
-	updateQty integer;
-	ingredRecord RECORD;
-	invenRecord RECORD;
-	mapRecord RECORD;
-	timeReceived time:= SYSDATE;
+updateQty integer;
+ingredRecord RECORD;
+invenRecord RECORD;
+mapRecord RECORD;
+timeReceived TIMESTAMP:= LOCALTIMESTAMP;
 
 BEGIN
-
---Get user id
 
 --find recipe record in mapPQ
 FOR mapRecord IN SELECT * FROM ingredient_recipe_map WHERE rec_name = recipe
 LOOP
-	RAISE NOTICE 'selected %', recipe;
-	RAISE NOTICE 'userID is %', userName;
-
 --loop for each ingredient
-	FOR invenRecord IN
-	SELECT * INTO ingredRecord FROM inventory i JOIN ingredient_recipe_map m
+    FOR invenRecord IN (
+    SELECT * FROM inventory i JOIN ingredient_recipe_map m
 	ON m.ingred_name =  i.ingred_name
-	WHERE m.rec_name = recipe
+	WHERE m.rec_name = recipe )
 	LOOP
-
-		IF(invenRecord.inventory_qty < ingredRecord.recipe_qty)THEN
-			RAISE EXCEPTION 'Cannot fulfill order: insufficient quantity of %', m.ingred_name;
-		ELSE
-			updateQty := ingredRecord.inventory_qty - ingredRecord.recipe_qty;
-			--Decrement quantity
-			UPDATE inventory SET inventory_qty = updateQty
-			WHERE ingred_name = ingredRecord.ingred_name;
-
-		RAISE NOTICE 'joined on %', recipe;
-		RAISE NOTICE 'ingredient is %', i.ingred_name;
+        IF (invenRecord.inventory_qty < invenRecord.recipe_qty) THEN
+            RAISE EXCEPTION 'Cannot fulfill order: insufficient quantity of %', invenRecord.ingred_name;
+        ELSE
+            UPDATE inventory SET inventory_qty = invenRecord.inventory_qty - invenRecord.recipe_qty
+			WHERE ingred_name = invenRecord.ingred_name;
+        END IF;
 	END LOOP;
-
 END LOOP;
 
-RETURN passed;
+INSERT INTO orders (user_id, time_placed, rec_name) VALUES (userName, timeReceived, recipe);
+RAISE NOTICE 'Order Complete!';
+RETURN;
 END;
 $$ LANGUAGE plpgsql;
+
+
+
+
+
+
